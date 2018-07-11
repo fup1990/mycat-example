334d3cd35b2718ad8af9f3b76a409531f6a7632a
fup1990 <fup1990@163.com>
2018-07-11 10:33:15 +0800

    mycat-example

---
+++ src/main/java/com/gome/fup/example/mycat/MycatExampleApplication.java
@@ -0,0 +1,12 @@
+package com.gome.fup.example.mycat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycatExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycatExampleApplication.class, args);
+	}
+}
